e476e9e29b6f63fa8911628af86bc62f6ab90ee9
Michael Kutz <michael.kutz@rewe-digital.com>
2018-11-06 16:06:38 +0100

    Use GOOGLE_CHAT_URL environment variable as default URL

--- vars/sendGoogleChat.groovy
+++ vars/sendGoogleChat.groovy
@@ -2,7 +2,7 @@
 
 import static groovy.json.JsonOutput.toJson
 
-void call(final String simpleTextMessage, final String url) {
+void call(final String simpleTextMessage, final String url = env.GOOGLE_CHAT_URL) {
     final String body = [text: simpleTextMessage]
     httpRequest(requestBody: toJson(body),
         url: url, httpMode: 'POST', contentType: 'APPLICATION_JSON_UTF8')
