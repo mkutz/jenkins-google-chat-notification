0cfe57e37fb6f309e69caa0a3caecd50ed3b806e
Michael Kutz <michael.kutz@rewe-digital.com>
2018-11-06 16:08:11 +0100

    Output request body for debugging

--- vars/sendGoogleChat.groovy
+++ vars/sendGoogleChat.groovy
@@ -3,7 +3,7 @@
 import static groovy.json.JsonOutput.toJson
 
 void call(final String simpleTextMessage, final String url = env.GOOGLE_CHAT_URL) {
-    final String body = [text: simpleTextMessage]
-    httpRequest(requestBody: toJson(body),
-        url: url, httpMode: 'POST', contentType: 'APPLICATION_JSON_UTF8')
+    final String requestBody = toJson([text: simpleTextMessage]
+    echo requestBody
+    httpRequest(requestBody: requestBody), url: url, httpMode: 'POST', contentType: 'APPLICATION_JSON_UTF8')
 }
